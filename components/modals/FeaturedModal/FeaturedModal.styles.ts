78041a5e95635594fea43db0d32db091fe09db1f
NizTheDev <arehman08@hotmail.com>
2022-05-27 21:54:51 +0500

    removed backdrop elements from individual modals

--- components/modals/FeaturedModal/FeaturedModal.styles.ts
+++ components/modals/FeaturedModal/FeaturedModal.styles.ts
@@ -1,17 +1,5 @@
 import styled from "styled-components";
 
-
-export const FeaturedModalElement = styled.div`
-  display: flex;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  position: fixed;
-  z-index: 3;
-  background-color: rgba(200, 200, 200, 0.8);
-`;
-
 export const FeaturedModalImageContainerElement = styled.div`
   height: 128px;
   width: 128px;
